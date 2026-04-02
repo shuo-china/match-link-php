007e12f33a8bc2dc65e908d9a90ba60f6d32465d
aaron <china.jinshuo@gmail.com>
2026-04-02 14:28:51 +0800

    dailt

--- app/portal/controller/EntryController.php
+++ app/portal/controller/EntryController.php
@@ -8,14 +8,6 @@ class EntryController extends BaseController
 {
     public function index()
     {
-        $members = \think\facade\Db::table('kr_member_before')->select()->toArray();
-
-        for ($i = 0; $i < count($members); $i++) {
-            $item = $members[$i];
-
-            Member::where('uid', $item['uid'])->update([
-                'vipLevel' => $item['vipLevel'],
-            ]);
-        }
+        $this->success(200, 'hello world');
     }
 }
